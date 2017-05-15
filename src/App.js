de0a68059ab109307298f07d7d529ff695cbb8dc
Natalia Kulas <nataliakulas@gmail.com>
2017-05-15 18:16:21 +0200

    basic template of api created;

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {getWeatherData, getLocationCoords} from './Api'
 
 import './App.css';
 
@@ -21,10 +22,38 @@ class App extends Component {
                 <div className="forecast-box">
                     <h1 className="city-name">{this.state.city}</h1>
                     <h2 className="country">{this.state.country}</h2>
-                    <h3 className="temperature">{this.state.currentTemperature} &#176;{this.state.currentUnit} <span className="super-small">/ {this.state.availableUnit}</span></h3>
+                    <h3 className="temperature">{this.state.currentTemperature} &#176;{this.state.currentUnit} <span
+                        className="super-small">/ {this.state.availableUnit}</span></h3>
                     <h2>{this.state.currentWeather}</h2>
                 </div>
             </div>
+        );
+    }
+
+    fetchWeather(units) {
+        getLocationCoords().then(
+            (coords) => {
+                // Got location data, get weather now
+                getWeatherData(units, coords).then(
+                    (weatherData) => {
+                        // Got weather data, proceed to update state using setState()
+                        this.setState({
+                            city: weatherData.body.name,
+                            country: weatherData.body.sys.country,
+                            currentTemperature: weatherData.body.main.temp,
+                            currentWeather: weatherData.body.weather[0].main,
+                            currentUnit: units,
+                            availableUnit: units === 'C' ? 'F' : 'C'
+                        });
+                    }, (error) => {
+                        // Could not get weather data.
+                        console.error(error);
+                    }
+                );
+            }, (error) => {
+                // Could not get location data.
+                console.error(error);
+            }
         );
     }
 }
