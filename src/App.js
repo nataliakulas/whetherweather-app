eab9e6dafc99ed0edcf082b3399d2226a01d8e35
Natalia Kulas <nataliakulas@gmail.com>
2017-05-15 17:44:07 +0200

    basic styles added; first draft of app created;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App"></div>
-    );
-  }
+    constructor() {
+        super(); // A call to the class we are extending: React.Component
+        this.state = {
+            city: 'Loading...',
+            country: 'Loading...',
+            currentWeather: 'Loading...',
+            currentTemperature: 0,
+            currentUnit: 'C',
+            availableUnit: 'F'
+        }
+    }
+
+    render() {
+        return (
+            <div className="main-wrapper overlay">
+                <div className="forecast-box">
+                    <h1 className="city-name">{this.state.city}</h1>
+                    <h2 className="country">{this.state.country}</h2>
+                    <h3 className="temperature">{this.state.currentTemperature} &#176;{this.state.currentUnit} <span className="super-small">/ {this.state.availableUnit}</span></h3>
+                    <h2>{this.state.currentWeather}</h2>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
