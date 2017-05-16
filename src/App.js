5979d8147e6e17f59d658d2c80bf0c324b8e31e0
Natalia Kulas <nataliakulas@gmail.com>
2017-05-16 11:35:09 +0200

    simple database added;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 
 const lastUpdate = new Date().toString();
 
+const cities = {
+    'warszawa': '52.229, 21.012',
+    'gdansk': '54.352, 18.646',
+    'sopot': '54.441, 18.560',
+    'gdynia': '54.519, 18.530',
+    'bydgoszcz': '53.123, 18.008',
+    'torun': '53.014, 18.598',
+    'poznan': '52.406, 16.925',
+    'szczecin': '53.428, 14.553',
+    'swinoujscie': '53.910, 14.247'
+}
+
 class App extends Component {
     render() {
         return (
