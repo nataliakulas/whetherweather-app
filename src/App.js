d524af6d2e750fb234b080355d193c14802206bc
Natalia Kulas <nataliakulas@gmail.com>
2017-05-16 11:21:41 +0200

    updated at added;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 
+const lastUpdate = new Date().toString();
+
 class App extends Component {
     render() {
         return (
             <div>
-
+                <h1>This is Whether Weather App!</h1>
+                <p>Last updated at: {lastUpdate}</p>
             </div>
 
         );
