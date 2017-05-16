7ccb189e826dc74628893f7594350406c4730722
Natalia Kulas <nataliakulas@gmail.com>
2017-05-16 14:34:33 +0200

    cities moved to separate json;

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import React, {Component} from 'react';
 import Grid from 'grid-styled'
 import {Title, Wrapper, Button, Paragraph} from './components/styled';
 
-const lastUpdate = new Date().toString();
+import cities from '../public/data/cities.json'
 
-const cities = {
-    'warszawa': '52.229, 21.012',
-    'gdansk': '54.352, 18.646',
-    'sopot': '54.441, 18.560',
-    'gdynia': '54.519, 18.530',
-    'bydgoszcz': '53.123, 18.008',
-    'poznan': '52.406, 16.925',
-    'torun': '53.014, 18.598',
-    'szczecin': '53.428, 14.553',
-    'swinoujscie': '53.910, 14.247'
-};
+const lastUpdate = new Date().toString();
 
 class App extends Component {
     render() {
