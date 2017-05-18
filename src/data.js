d7d21865962c854405d90485b934c460de240b64
Natalia Kulas <nataliakulas@gmail.com>
2017-05-18 15:27:21 +0200

    data js made simplier;

--- src/data.js
+++ src/data.js
@@ -3,56 +3,47 @@ export default {
         {
             "id": 1,
             "name": "warszawa",
-            "latitude": 52.229,
-            "longitude": 21.012
+            "coordinates": "52.229,21.012"
         },
         {
             "id": 2,
             "name": "gdańsk",
-            "latitude": 54.352,
-            "longitude": 18.646
+            "coordinates": "54.352,18.646"
         },
         {
             "id": 3,
             "name": "sopot",
-            "latitude": 54.441,
-            "longitude": 18.560
+            "coordinates": "54.441,18.560"
         },
         {
             "id": 4,
             "name": "gdynia",
-            "latitude": 54.519,
-            "longitude": 18.530
+            "coordinates": "54.519,18.530"
         },
         {
             "id": 5,
             "name": "bydgoszcz",
-            "latitude": 53.123,
-            "longitude": 18.008
+            "coordinates": "53.123,18.008"
         },
         {
             "id": 6,
             "name": "toruń",
-            "latitude": 53.014,
-            "longitude": 18.598
+            "coordinates": "53.014,18.598"
         },
         {
             "id": 7,
             "name": "poznań",
-            "latitude": 52.406,
-            "longitude": 16.925
+            "coordinates": "52.406,16.925"
         },
         {
             "id": 8,
             "name": "szczecin",
-            "latitude": 53.428,
-            "longitude": 14.553
+            "coordinates": "53.428,14.553"
         },
         {
             "id": 9,
             "name": "świnoujście",
-            "latitude": 53.910,
-            "longitude": 14.247
+            "coordinates": "53.910,14.247"
         }
     ]
 }
