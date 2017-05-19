ff3370f9a7fbfcf8f661dab03fd7321d9ce06201
Natalia Kulas <nataliakulas@gmail.com>
2017-05-19 09:00:49 +0200

    api changed back and fixed;

--- src/ajax/Api.js
+++ src/ajax/Api.js
@@ -1,6 +1,6 @@
 import data from '../../public/data/cities.json'
 
-export const url = "http://api.apixu.com/v1/current.json?key=";
+export const url = "https://api.apixu.com/v1/current.json?key=";
 export const key = "b18b14ba85b043e89db130626171705";
 
 export let name = () => {
