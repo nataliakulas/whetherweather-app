875b998d975fe71112ba777bccfdf1d0edf77686
Natalia Kulas <nataliakulas@gmail.com>
2017-05-22 17:50:16 +0200

    data moved to separate file; data moved to src directory; unused data from cities removed;

--- src/App.js
+++ src/App.js
@@ -8,24 +8,8 @@ import {url, key} from './ajax/Api.js'
 
 import './assets/global.css'
 
-import data from '../public/data/cities.json'
-
-import N from '../public/images/icons/wind/N.png'
-import NNE from '../public/images/icons/wind/NNE.png'
-import NE from '../public/images/icons/wind/NE.png'
-import ENE from '../public/images/icons/wind/ENE.png'
-import E from '../public/images/icons/wind/E.png'
-import ESE from '../public/images/icons/wind/ESE.png'
-import SE from '../public/images/icons/wind/SE.png'
-import SSE from '../public/images/icons/wind/SSE.png'
-import S from '../public/images/icons/wind/S.png'
-import SSW from '../public/images/icons/wind/SSW.png'
-import SW from '../public/images/icons/wind/SW.png'
-import WSW from '../public/images/icons/wind/WSW.png'
-import W from '../public/images/icons/wind/W.png'
-import WNW from '../public/images/icons/wind/WNW.png'
-import NW from '../public/images/icons/wind/NW.png'
-import NNW from '../public/images/icons/wind/NNW.png'
+import data from './data/city-data.json'
+import windData from './data/wind-data'
 
 class App extends Component {
     constructor() {
@@ -77,52 +61,52 @@ class App extends Component {
         let windDir;
         switch (this.state.weatherData.wind_dir) {
             case 'N':
-                windDir = N;
+                windDir = windData.N;
                 break;
             case 'NNE':
-                windDir = NNE;
+                windDir = windData.NNE;
                 break;
             case 'NE':
-                windDir = NE;
+                windDir = windData.NE;
                 break;
             case 'ENE':
-                windDir = ENE;
+                windDir = windData.ENE;
                 break;
             case 'E':
-                windDir = E;
+                windDir = windData.E;
                 break;
             case 'ESE':
-                windDir = ESE;
+                windDir = windData.ESE;
                 break;
             case 'SE':
-                windDir = SE;
+                windDir = windData.SE;
                 break;
             case 'SSE':
-                windDir = SSE;
+                windDir = windData.SSE;
                 break;
             case 'S':
-                windDir = S;
+                windDir = windData.S;
                 break;
             case 'SSW':
-                windDir = SSW;
+                windDir = windData.SSW;
                 break;
             case 'SW':
-                windDir = SW;
+                windDir = windData.SW;
                 break;
             case 'WSW':
-                windDir = WSW;
+                windDir = windData.WSW;
                 break;
             case 'W':
-                windDir = W;
+                windDir = windData.W;
                 break;
             case 'WNW':
-                windDir = WNW;
+                windDir = windData.WNW;
                 break;
             case 'NW':
-                windDir = NW;
+                windDir = windData.NW;
                 break;
             case 'NNW':
-                windDir = NNW;
+                windDir = windData.NNW;
                 break;
             default:
                 windDir = '';
